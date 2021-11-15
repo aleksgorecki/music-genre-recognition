dcb6b13f1b6cb5c3ecb94f9f8e4e4fe754be73a9
Aleksander Górecki <goreckialeksander9@gmail.com>
2021-11-15 03:11:12 +0100

    Complete function definitions for audio processing

--- audio_processing.py
+++ audio_processing.py
@@ -1,11 +1,14 @@
 import numpy as np
 import numpy.typing
+import typing
 import librosa
 import librosa.core
-import typing
+from dataclasses import dataclass
 
 
+@dataclass
 class LibrosaMonoTimeseries:
+
     timeseries: numpy.typing.ArrayLike
     sr: int
 
@@ -14,14 +17,16 @@ class LibrosaMonoTimeseries:
         self.sr = librosa_timeseries_tuple[1]
 
 
-def load_audio_and_convert_to_mono(file_path: str,
-                                   sr_overwrite: typing.Optional[int] = None
-                                   ) -> LibrosaMonoTimeseries:
+def load_audio_file_and_convert_to_mono(file_path: str,
+                                        sr_overwrite: typing.Optional[int] = None,
+                                        offset_sec: float = 0.0,
+                                        duration: float = None
+                                        ) -> LibrosaMonoTimeseries:
     if sr_overwrite is not None:
         sr = sr_overwrite
     else:
         sr = 22050
-    audio_data = librosa.core.load(path=file_path, sr=sr, mono=True)
+    audio_data = librosa.core.load(path=file_path, sr=sr, mono=True, offset=offset_sec, duration=duration)
     return LibrosaMonoTimeseries(audio_data)
 
 
@@ -31,10 +36,10 @@ def mel_spectrogram_from_timeseries(audio_data: LibrosaMonoTimeseries,
                                     log_scale: bool = True
                                     ) -> typing.Any:
     if sr_overwrite is not None:
-        fs = sr_overwrite
+        sr = sr_overwrite
     else:
-        fs = audio_data.sr
-    mel_spec = librosa.feature.melspectrogram(y=audio_data.timeseries, sr=fs, n_mel=mel_bands)
+        sr = audio_data.sr
+    mel_spec = librosa.feature.melspectrogram(y=audio_data.timeseries, sr=sr, n_mel=mel_bands)
     if log_scale:
         mel_spec = librosa.core.power_to_db(mel_spec, ref=np.max)
     return mel_spec
@@ -42,7 +47,6 @@ def mel_spectrogram_from_timeseries(audio_data: LibrosaMonoTimeseries,
 
 def pad_timeseries(audio_data: LibrosaMonoTimeseries,
                    target_length: int,
-                   padding_value: float = 0
                    ) -> LibrosaMonoTimeseries:
     num_samples_to_add = int(target_length - len(audio_data.timeseries))
     padded_timeseries = np.pad(audio_data.timeseries, (0, num_samples_to_add))
@@ -50,17 +54,33 @@ def pad_timeseries(audio_data: LibrosaMonoTimeseries,
 
 
 def split_timeseries(audio_data: LibrosaMonoTimeseries,
-                     fragment_duration_ms: int,
+                     fragment_duration_sec: float,
                      pad_incomplete: bool = False
                      ) -> typing.List[LibrosaMonoTimeseries]:
-    fragment_duration_in_samples = int(fragment_duration_ms * audio_data.sr / 1000)
-    num_fragments = int(len(audio_data.timeseries) / fragment_duration_in_samples)
-    for i in range(0, num_fragments):
-        pass
+    fragment_duration_in_samples = int(fragment_duration_sec * audio_data.sr)
+    splits = list()
+    for i in range(0, len(audio_data.timeseries), fragment_duration_in_samples):
+        split = np.ndarray(audio_data.timeseries[i:i + fragment_duration_in_samples],
+                           dtype=audio_data.timeseries.dtype)
+        splits.append(LibrosaMonoTimeseries((split, audio_data.sr)))
+
+    num_fragments = len(splits)
+    if pad_incomplete and fragment_duration_in_samples*num_fragments < len(audio_data.timeseries):
+        incomplete_split = np.ndarray(audio_data.timeseries[fragment_duration_in_samples*num_fragments:],
+                                      dtype=audio_data.timeseries.dtype)
+        padded_split = pad_timeseries(LibrosaMonoTimeseries((incomplete_split, audio_data.sr)),
+                                      fragment_duration_in_samples)
+        splits.append(padded_split)
+
+    return splits
 
 
 def get_fragment_of_timeseries(audio_data: LibrosaMonoTimeseries,
-                               fragment_duration_ms: int
+                               offset_sec: float,
+                               fragment_duration_sec: float
                                ) -> LibrosaMonoTimeseries:
-    fragment_duration_in_samples = int(fragment_duration_ms) * audio_data.sr
-    pass
+    fragment_duration_in_samples = int(fragment_duration_sec * audio_data.sr)
+    offset_in_samples = int(offset_sec * audio_data.sr)
+    fragment = np.ndarray(audio_data.timeseries[offset_in_samples:offset_in_samples + fragment_duration_in_samples],
+                          dtype=audio_data.timeseries.dtype)
+    return LibrosaMonoTimeseries((fragment, audio_data.sr))
